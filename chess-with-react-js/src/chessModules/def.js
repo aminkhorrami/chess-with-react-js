5aed839bc4682dbadd423d708c117a0865edfbef
aminkhorrami <a.khorrami71@gmail.com>
2021-04-28 12:37:19 +0430

    updated : UI added.

---
+++ chess-with-react-js/src/chessModules/def.js
@@ -0,0 +1,1 @@
+const Pieces = { EMPTY: 0, wP: 1, wN: 2, wB: 3 };
---
+++ chess-with-react-js/src/chessUIcomponents/index.js
@@ -0,0 +1,11 @@
+export default function UI() {
+  return (
+    <div id="FenInDev">
+      Fen : <br />
+      <input type="text" id="fenIn" />
+      <button type="button" id="SetFen">
+        Set Position{" "}
+      </button>
+    </div>
+  );
+}
